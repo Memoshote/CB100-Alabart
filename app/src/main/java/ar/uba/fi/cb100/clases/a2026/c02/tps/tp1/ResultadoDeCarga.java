1857df05f7dc73b394016e9520adeca8c8fd4ad1
Thomas <thomas.alabart@gmail.com>
2026-09-08 19:42:22 -0300

    Agregar LineaInvalidaException y ResultadoDeCarga

---
+++ app/src/main/java/ar/uba/fi/cb100/clases/a2026/c02/tps/tp1/ResultadoDeCarga.java
@@ -0,0 +1,8 @@
+package ar.uba.fi.cb100.clases.a2026.c02.tps.tp1;
+
+public record ResultadoDeCarga(
+        RegistroDePrestamos registro,
+        String[] errores,
+        int lineasDeDatos
+) {
+}
